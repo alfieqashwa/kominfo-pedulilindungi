7fb2a4754452bfdaa7efc52f98c940dda813b73b
alfieqashwa <alfieqashwa@gmail.com>
2020-09-23 04:12:36 +0700

    [TEGAL] --on progress

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import urllib
 from datetime import datetime
 import json
 
@@ -476,7 +477,7 @@ class PurbalinggaSpider(scrapy.Spider):
         "https://petatematik.purbalinggakab.go.id/api/corona/data/geoJSON"
     ]
     headers = {
-        "Accept": "Accept: application/json, text/javascript, */*; q=0.01",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9",
         "Referer": "https://petatematik.purbalinggakab.go.id/peta/monitoring-corona",
@@ -519,5 +520,104 @@ class PurbalinggaSpider(scrapy.Spider):
                 'odr_total': '',
                 'total_pp': '',
                 'total_ppdt': '',
+                'source_link': source_link,
+            }
+
+
+################################# KELURAHAN #####################################
+
+
+class TegalSpider(scrapy.Spider):
+    name = "tegal"
+    start_urls = "https://corona.tegalkota.go.id/index.php/Api/api_getDataPetaCovid"
+
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://corona.tegalkota.go.id/?page=peta",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (X11 Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+    }
+
+# there's information about `kelurahan` only but no `kecamatan` classification.
+# source:
+# https://id.wikipedia.org/wiki/Daftar_kecamatan_dan_kelurahan_di_Kota_Tegal
+
+    margadana = ["Cabawan", "Kaligangsa", "Kalinyamat Kulon",
+                 "Krandon", "Margadana", "Pesurungan Lor", "Sumurpanggang"]
+    tegalbarat = ["Debong Lor", "Kemandungan", "Kraton",
+                  "Muarareja", "Pekauman", "Pesurungan Kidul", "Tegalsari"]
+    tegalselatan = ["Bandung", "Debong Kidul", "Debong Kulon",
+                    "Debong Tengah", "Kalinyamat Wetan", "Keturen", "Randugunting", "Tunon"]
+    tegaltimur = ["Kejambon", "Mangkukusuman",
+                  "Mintaragen", "Panggung", "Slerok"]
+
+    def start_requests(self):
+        params = {
+            # Query String Paramaters on Headers is required,
+            # otherwise the response status would be "ERROR".
+            'token': '4yt6rul1232y2yi23'
+        }
+
+        # Reminder:
+        # f'some string {some_variables}' in Python
+        # are similar as
+        # `some strings ${someVariables}` in JavaScript.
+
+        url = f'{self.start_urls}?{urllib.parse.urlencode(params)}'
+        request = scrapy.Request(url=url, callback=self.parse)
+        request.meta['params'] = params
+        return [request]
+
+    def parse(self, response):
+        raw_data = response.body
+        datas = json.loads(raw_data)
+        scrape_date = datetime.now().strftime("%Y-%m-%d")
+        types = 'kelurahan'
+        user_pic = 'Alfie Qashwa'
+        date_update = datetime.now().strftime("%Y-%m-%d")
+        provinsi = 'Jawa Tengah'
+        kabkot = 'Tegal'
+        source_link = 'https://corona.tegalkota.go.id/'
+
+        for data in datas["response"]["data"]:
+            kelurahan = data["nama_kelurahan"]
+            if kelurahan in self.margadana:
+                kecamatan = 'Marganda'
+            elif kelurahan in self.tegalbarat:
+                kecamatan = 'Tegal Barat'
+            elif kelurahan in self.tegalselatan:
+                kecamatan = 'Tegal Selatan'
+            elif kelurahan in self.tegaltimur:
+                kecamatan = 'Tegal Timur'
+            else:
+                kecamatan = ''
+            yield {
+                'scrape_date': scrape_date,
+                'types': types,
+                'user_pic': user_pic,
+                'date_update': date_update,
+                'provinsi': provinsi,
+                'kabkot': kabkot,
+                'kecamatan': kecamatan,
+                'kelurahan': kelurahan,
+
+                ############# // TODOS //#############
+
+                # 'alamat': '',
+                # 'total_odp':,
+                # 'total_pdp':,
+                # 'total_positif':,
+                # 'positif_sembuh':,
+                # 'positif_dirawat':,
+                # 'positif_isolasi': '',
+                # 'positif_meninggal':,
+                # 'total_otg': '',
+                # 'odr_total': '',
+                # 'total_pp': '',
+                # 'total_ppdt': '',
                 'source_link': source_link,
             }
