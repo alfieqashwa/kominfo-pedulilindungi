464219fa59c0b38ab05b51718dd70e59d2fa21b8
alfieqashwa <alfieqashwa@gmail.com>
2020-09-24 18:20:52 +0700

    [Done]--Kab. Kendal

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -717,3 +717,62 @@ class KotaMagelangSpider(scrapy.Spider):
                 'total_ppdt': '',
                 'source_link': source_link,
             }
+
+
+class KendalSpider(scrapy.Spider):
+    name = "kendal"
+    start_urls = [
+        "https://corona.kendalkab.go.id/"
+    ]
+
+    months = dict(Januari='01', Februari='02', Maret='03', April='04', Mei='05', Juni='06',
+                  Juli='07', Agustus='08', September='09', Oktober='10', November='11', Desember='12')
+
+    def parse(self, response):
+        scrape_date = datetime.now().strftime("%Y-%m-%d")
+        types = 'kecamatan'
+        user_pic = 'Alfie Qashwa'
+        crawl_date = response.xpath(
+            '/html/body/div[1]/section[2]/div[1]/div/div/div[1]/p/text()')[4].re(r', (\w+) (\w+) (\w+)')
+        day = crawl_date[0]
+        month = crawl_date[1]
+        year = crawl_date[2]
+        date_update = year + '-' + self.months[month] + '-' + day
+        provinsi = 'Jawa Tengah'
+        kabkot = 'Kendal'
+        kecamatan = response.xpath(
+            '//*[@id="example1"]/tbody/tr/td[2]/text()').getall()
+        total_odp = response.xpath(
+            '//*[@id="example1"]/tbody/tr/td[5]/text()').getall()
+        total_pdp = response.xpath(
+            '//*[@id="example1"]/tbody/tr/td[6]/text()').getall()
+        total_positif = response.xpath(
+            '//*[@id="example1"]/tbody/tr/td[7]/text()').getall()
+        odr_total = response.xpath(
+            '//*[@id="example1"]/tbody/tr/td[4]/text()').getall()
+        source_link = 'https://corona.kendalkab.go.id/'
+
+        for q in range(len(kecamatan)):
+            yield {
+                'scrape_date': scrape_date,
+                'types': types,
+                'user_pic': user_pic,
+                'date_update': date_update,
+                'provinsi': provinsi,
+                'kabkot': kabkot,
+                'kecamatan': kecamatan[q].capitalize().strip(),
+                'kelurahan': '',
+                'alamat': '',
+                'total_odp': total_odp[q].strip(),
+                'total_pdp': total_pdp[q].strip(),
+                'total_positif': total_positif[q].strip(),
+                'positif_sembuh': '',
+                'positif_dirawat': '',
+                'positif_isolasi': '',
+                'positif_meninggal': '',
+                'total_otg': '',
+                'odr_total': odr_total[q].strip(),
+                'total_pp': '',
+                'total_ppdt': '',
+                'source_link': source_link,
+            }
