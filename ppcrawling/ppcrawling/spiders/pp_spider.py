5140d5e07943eee5e7f86c2030f6e2d510f7a43d
alfieqashwa <alfieqashwa@gmail.com>
2020-10-08 21:11:10 +0700

    updated crawling except kotamagelang

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -558,7 +558,8 @@ class TegalSpider(scrapy.Spider):
             # Query String Paramaters on Headers is required,
             # otherwise the response status would be "ERROR".
             # 'token': '4yt6rul1232y2y1y5'
-            'token': '4yt6rul1232y2y1y6'
+            # 'token': '4yt6rul1232y2y1y6'
+            'token': '4yt6rul1232y2y1yp'
         }
 
         # Reminder:
