073b40ca76283e71e5e5b6f4e10ef71322fb081e
alfieqashwa <alfieqashwa@gmail.com>
2020-10-17 21:13:50 +0700

    updated crawling except banjarnegara -- the site is down

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -561,7 +561,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2y1y12'
             # 'token': '4yt6rul1232y2y1y14'
             # 'token': '4yt6rul1232y2y1y15'
-            'token': '4yt6rul1232y2y1y16'
+            # 'token': '4yt6rul1232y2y1y16'
+            'token': '4yt6rul1232y2y1y1k'
         }
 
         # Reminder:
