1f09905537e05b7812518515e0979fcdf70d984a
alfieqashwa <alfieqashwa@gmail.com>
2020-11-18 20:47:36 +0700

    updated crawll-- except kudus

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -528,7 +528,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2y1114'
             # 'token': '4yt6rul1232y2y1115'
             # 'token': '4yt6rul1232y2y1116'
-            'token': '4yt6rul1232y2y111k'
+            # 'token': '4yt6rul1232y2y111k'
+            'token': '4yt6rul1232y2y111p'
         }
 
         # Reminder:
