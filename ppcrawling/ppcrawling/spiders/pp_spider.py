f2ff9cb656bf87dc0616956c845582730f33ca56
alfieqashwa <alfieqashwa@gmail.com>
2020-12-08 19:45:32 +0700

    Updated All except Semarang

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -546,7 +546,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2y124'
             # 'token': '4yt6rul1232y2y125'
             # 'token': '4yt6rul1232y2y126'
-            'token': '4yt6rul1232y2y12k'
+            # 'token': '4yt6rul1232y2y12k'
+            'token': '4yt6rul1232y2y12p'
         }
 
         # Reminder:
