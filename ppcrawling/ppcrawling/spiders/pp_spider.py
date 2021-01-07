18caebf68794957a9d7530b551cb5211913d586a
alfieqashwa <alfieqashwa@gmail.com>
2021-01-07 20:25:12 +0700

    scraping has been updated!!

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -575,7 +575,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2113'
             # 'token': '4yt6rul1232y2114'
             # 'token': '4yt6rul1232y2115'
-            'token': '4yt6rul1232y2116'
+            # 'token': '4yt6rul1232y2116'
+            'token': '4yt6rul1232y211k'
         }
 
         # Reminder:
