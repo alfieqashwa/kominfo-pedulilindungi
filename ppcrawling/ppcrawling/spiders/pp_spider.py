619bc0041914bd0da5ad19dafc08200679ae0f24
alfieqashwa <alfieqashwa@gmail.com>
2021-01-19 13:13:32 +0700

    updated except banjarnegara

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -586,7 +586,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y21115'
             # 'token': '4yt6rul1232y21116'
             # 'token': '4yt6rul1232y2111k'
-            'token': '4yt6rul1232y2111p'
+            # 'token': '4yt6rul1232y2111p'
+            'token': '4yt6rul1232y2111i'
         }
 
         # Reminder:
