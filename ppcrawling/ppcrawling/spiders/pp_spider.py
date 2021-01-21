efe20c1952778fab3c3c0c2675c79752e538714d
alfieqashwa <alfieqashwa@gmail.com>
2021-01-21 21:24:05 +0700

    re-created banjarnegara script

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -722,25 +722,65 @@ class KendalSpider(scrapy.Spider):
                 'source_link': source_link,
             }
 
-# TODO
-# class BanjarnegaraSpider(scrapy.Spider):
-#     name = "banjarnegara"
-#     start_urls = [
-#         "http://corona.banjarnegarakab.go.id/"
-#     ]
 
-#     def parse(self, response):
-#         scrape_date = datetime.now().strftime("%Y-%m-%d")
-#         types = 'kabupaten'
-#         user_pic = 'Alfie Qashwa'
-#         crawl_date = response.css(
-#             '#sebaran .mt-2::text').re(r': (\w+) (\w+) (\w+)')
-#         day = crawl_date[0]
-#         month = self.months[crawl_date[1]]
-#         year = crawl_date[2]
-#         date_update = year + '-' + month + '-' + day
-#         provinsi = 'Jawa Tengah'
-#         kabkot = 'Banjarnegara'
+class BanjarnegaraSpider(scrapy.Spider):
+    name = "banjarnegara"
+    start_urls = [
+        # "http://corona.banjarnegarakab.go.id/"
+        "https://corona.jatengprov.go.id/data"
+    ]
+
+    def parse(self, response):
+        scrape_date = datetime.now().strftime("%Y-%m-%d")
+        types = 'kabupaten'
+        user_pic = 'Alfie Qashwa'
+        date_update = scrape_date
+        provinsi = 'Jawa Tengah'
+        kabkot = 'Banjarnegara'
+        kecamatan = ''
+        kelurahan = ''
+        alamat = ''
+        total_odp = response.xpath(
+            '//*[@id="pills-domisili3"]/div[1]/table/tbody/tr[30]/td[6]/text()').get()
+        total_pdp = ''
+        total_positif = response.xpath(
+            '//*[@id="pills-domisili3"]/div[1]/table/tbody/tr[30]/td[2]/text()').get()
+        positif_sembuh = response.xpath(
+            '//*[@id="pills-domisili3"]/div[1]/table/tbody/tr[30]/td[4]/text()').get()
+        positif_dirawat = response.xpath(
+            '//*[@id="pills-domisili3"]/div[1]/table/tbody/tr[30]/td[3]/text()').get()
+        positif_isolasi = ''
+        positif_meninggal = response.xpath(
+            '//*[@id="pills-domisili3"]/div[1]/table/tbody/tr[30]/td[5]/text()').get()
+        total_otg = ''
+        odr_total = ''
+        total_pp = ''
+        total_ppdt = ''
+        source_link = "http://corona.banjarnegarakab.go.id/"
+
+        yield {
+            'scrape_date': scrape_date,
+            'types': types,
+            'user_pic': user_pic,
+            'date_update': date_update,
+            'provinsi': provinsi,
+            'kabkot': kabkot,
+            'kecamatan': kecamatan,
+            'kelurahan': kelurahan,
+            'alamat': alamat,
+            'total_odp': total_odp.replace('.', ''),
+            'total_pdp': total_pdp,
+            'total_positif': total_positif.replace('.', ''),
+            'positif_sembuh': positif_sembuh.replace('.', ''),
+            'positif_dirawat': positif_dirawat.replace('.', ''),
+            'positif_isolasi': positif_isolasi,
+            'positif_meninggal': positif_meninggal.replace('.', ''),
+            'total_otg': total_otg,
+            'odr_total': odr_total,
+            'total_pp': total_pp,
+            'total_ppdt': total_ppdt,
+            'source_link': source_link,
+        }
 
 # class BanjarnegaraSpider(scrapy.Spider):
 #     name = "banjarnegara"
