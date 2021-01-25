fff1a0765285125fba03976da868e35240794175
alfieqashwa <alfieqashwa@gmail.com>
2021-01-25 18:06:42 +0700

    has been updated....

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -592,7 +592,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y21121'
             # 'token': '4yt6rul1232y21122'
             # 'token': '4yt6rul1232y21123'
-            'token': '4yt6rul1232y21124'
+            # 'token': '4yt6rul1232y21124'
+            'token': '4yt6rul1232y21125'
         }
 
         # Reminder:
