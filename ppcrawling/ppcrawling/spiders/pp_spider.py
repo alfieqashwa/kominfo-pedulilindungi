ea2a7bfd9be8a37f4f601bf6760b76bbd800b5e8
alfieqashwa <alfieqashwa@gmail.com>
2021-02-22 19:44:34 +0700

    UPDATEd

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -619,7 +619,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2121k'
             # 'token': '4yt6rul1232y2121p'
             # 'token': '4yt6rul1232y2121i'
-            'token': '4yt6rul1232y21221'
+            # 'token': '4yt6rul1232y21221'
+            'token': '4yt6rul1232y21222'
         }
 
         # Reminder:
