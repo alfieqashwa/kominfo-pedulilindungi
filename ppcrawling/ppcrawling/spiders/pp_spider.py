4a92815521b5ff85ea9cf5ef82aa447531adf6a2
alfieqashwa <alfieqashwa@gmail.com>
2021-03-09 19:01:04 +0700

    crawling has been updated except grobogan

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -633,7 +633,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y2135'
             # 'token': '4yt6rul1232y2136'
             # 'token': '4yt6rul1232y213k'
-            'token': '4yt6rul1232y213p'
+            # 'token': '4yt6rul1232y213p'
+            'token': '4yt6rul1232y213i'
         }
 
         # Reminder:
