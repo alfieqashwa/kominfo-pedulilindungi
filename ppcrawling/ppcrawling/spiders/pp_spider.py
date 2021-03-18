7d95aed902dc024ef1107d23e7129a707894b807
alfieqashwa <alfieqashwa@gmail.com>
2021-03-18 19:08:15 +0700

    udapted crawl

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -640,7 +640,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y21314'
             # 'token': '4yt6rul1232y21315'
             # 'token': '4yt6rul1232y21316'
-            'token': '4yt6rul1232y2131k'
+            # 'token': '4yt6rul1232y2131k'
+            'token': '4yt6rul1232y2131p'
         }
 
         # Reminder:
