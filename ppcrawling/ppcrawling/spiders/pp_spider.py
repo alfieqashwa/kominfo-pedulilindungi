1e37a4c42ad8c570bf5a02812c0367e7397f1b42
alfieqashwa <alfieqashwa@gmail.com>
2021-04-20 17:09:03 +0700

    start updating again until the next month.

--- ppcrawling/ppcrawling/spiders/pp_spider.py
+++ ppcrawling/ppcrawling/spiders/pp_spider.py
@@ -666,7 +666,8 @@ class TegalSpider(scrapy.Spider):
             # 'token': '4yt6rul1232y21416'
             # 'token': '4yt6rul1232y2141k'
             # 'token': '4yt6rul1232y2141p'
-            'token': '4yt6rul1232y2141i'
+            # 'token': '4yt6rul1232y2141i'
+            'token': '4yt6rul1232y2142y'
         }
 
         # Reminder:
